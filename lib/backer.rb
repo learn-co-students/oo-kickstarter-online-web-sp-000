2f43dc518c2163903eeb6272d31a069e88035191
Joe Ganobsik <jganobsik@gmail.com>
2018-12-29 13:54:54 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer
+  
+  attr_accessor :backed_projects, :name
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end 
+  
+  def back_project(project)
+    @backed_projects << project
+    if project.backers.include?(self) != true
+    project.add_backer(self)
+  end
+  end
+end
