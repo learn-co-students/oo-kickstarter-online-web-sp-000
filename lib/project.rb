7510e3ca3508539b803cc753e3b17346430b767e
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-11 16:48:25 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,24 @@
+class Project
+
+  attr_accessor :backed_projects
+
+def initialize(title)
+  @title = title
+  @backers = []
+
+end
+
+def backers
+  @backers
+end
+def title
+  @title
+end
+def add_backer(backer)
+  @backers << backer
+  backer.backed_projects << self
+
+end
+
+
+end
