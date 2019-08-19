75f302903199a112f10912ac137476c390dbd86f
Ismail Jalloh <ijalloh21@gmail.com>
2019-08-19 01:55:20 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,9 +8,13 @@ class Backer
     @backed_projects = []
   end
   
+  def backed_projects
+    @backed_projects
+  end
+  
   def back_project(project)
     @backed_projects << project
-    binding.pry
+    project.backers << self
   end
   
 end
