2b2b89bc55886db253e7447fce0bd1d8a48762a1
Adam Weissman <adam@comedytragedyepic.com>
2019-10-03 08:50:15 +0000

    Done.

--- lib/project.rb
+++ lib/project.rb
@@ -5,11 +5,13 @@ class Project
   def initialize(title)
     @backers = []
     @title = title
+    @car = "car"
   end
   
   def add_backer(a_person)
     @backers << a_person
     a_person.backed_projects << self
   end
+  
   
 end
