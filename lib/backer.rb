0663837bcdd5d5582ae34cc291ba7b490aaeb3c8
cantstopcoding <jonathan_koss@yahoo.com>
2020-01-23 20:16:21 -0500

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,23 @@
+require 'pry'
+
 class Backer
-    attr_reader :backed_projects
+    attr_accessor :name, :backed_projects
 
     def initialize(name)
         @name = name
         @backed_projects = []
     end 
+    
+    def back_project(project)
+        backed_projects << project
+        # it also adds the backer of the project to the project's backer
+        # I have to add the backer instance to the backers array (Project.backers)
+        project.add_backer(self) unless project.backers.include?(self)
+    end
 end 
+
+# logan = Backer.new("Logan")
+# hoverboard = Project.new("Awesome Hoverboard")
+# logan.back_project(hoverboard)
+# # expect(hoverboard.backers).to include(logan)
+
