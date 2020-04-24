5ecbb7dc9001b925496a687a78299e4b775a5cb3
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-24 19:58:47 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,14 @@
+class Backer
+  attr_reader :name, :backed_projects
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(name)
+    @backed_projects << name
+    name.backers << self
+  end
+  
+end
