ff8c03906e77c65865ad88f6eabe7926d4d195af
strinie <hello@katherinesalamat.com>
2020-06-08 21:48:33 -0400

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,14 @@
+class Backer
+    attr_accessor :name
+    attr_reader :backed_projects
   
-class Project
-    attr_accessor :title, :backers
-  
-    def initialize(title)
-        @title = title
-        @backers = []
+    def initialize(name)
+        @name = name
+        @backed_projects = []
     end
   
-    def add_backer(backer)
-        @backers << backer
-        backer.backed_projects << self
+    def back_project(project)
+        @backed_projects << project
+        project.backers << self 
     end
 end
