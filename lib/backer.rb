211d40248dcabf5fd950652e038cf3308316f554
Dean Erickson <dmepilot@yahoo.com>
2020-07-01 13:51:58 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+class Backer
+  attr_reader :name, :backed_projects
+  attr_accessor :backers
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+      project.backers<< self
+  end
+  
+end
