506c1900143d5c81781d9d781202aae0cc276978
Chris Baum <christopher.baum@valpo.edu>
2021-02-04 01:28:14 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,7 +6,11 @@ class Backer
   def initialize(name)
     @name = name
     @backed_projects = []
+  end
 
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
   end
 
 end
