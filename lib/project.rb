e9c1dddd76af33e48885fbd9e1c90268e5058207
James Harris <mharris9608@yahoo.com>
2021-03-20 21:34:41 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project
+  attr_accessor :backers, :title
+
+  def initialize(title)
+    @backers = []
+    @title = title
+  end
+
+  def add_backer(backer)
+    backer.backed_projects << self
+    @backers << backer
+  end
+end
